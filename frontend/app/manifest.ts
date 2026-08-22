d2ff66932281346e752ef103cbb687cfd1884416
Swayam chondigra <sbc25120@gmail.com>
2026-08-22 18:16:12 +0530

    Add GanaSuno app shortcuts

--- frontend/app/manifest.ts
+++ frontend/app/manifest.ts
@@ -22,5 +22,43 @@ export default function manifest(): MetadataRoute.Manifest {
         type: "image/png",
       },
     ],
+    shortcuts: [
+      {
+        name: "1990s Era",
+        short_name: "1990s",
+        description: "Listen to nostalgic 1990s songs",
+        url: "/?era=1990s",
+        icons: [
+          {
+            src: "/icons/icon-192.png",
+            sizes: "192x192",
+          },
+        ],
+      },
+      {
+        name: "2000s Era",
+        short_name: "2000s",
+        description: "Listen to 2000s music",
+        url: "/?era=2000s",
+        icons: [
+          {
+            src: "/icons/icon-192.png",
+            sizes: "192x192",
+          },
+        ],
+      },
+      {
+        name: "2010s Era",
+        short_name: "2010s",
+        description: "Listen to 2010s music",
+        url: "/?era=2010s",
+        icons: [
+          {
+            src: "/icons/icon-192.png",
+            sizes: "192x192",
+          },
+        ],
+      },
+    ],
   };
 }
